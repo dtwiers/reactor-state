92ce7177ec934b625abdf81fec25a0e860d96df4
Derek Wiers <dtwiers08@gmail.com>
2021-07-21 09:40:39 -0500

    added action$

--- src/core.ts
+++ src/core.ts
@@ -1,4 +1,4 @@
-import { BehaviorSubject } from "rxjs";
+import { BehaviorSubject, ReplaySubject, Subject } from "rxjs";
 import {
   ActionFunction,
   ActionObject,
@@ -8,20 +8,25 @@ import {
 } from "./types";
 
 const createActionFunction =
-  <T>(state$: BehaviorSubject<T>) =>
-  <A extends unknown[]>(fn: ReducerFunction<T, A>): ActionFunction<A> =>
-  (...params) =>
+  <T>(state$: BehaviorSubject<T>, action$: ReplaySubject<unknown>) =>
+  <A extends unknown[]>(key: string, fn: ReducerFunction<T, A>): ActionFunction<A> =>
+  (...params) => {
+    action$.next({category: "before", type: key, params: params})
     fn(state$, ...params);
+    action$.next({category: "after", type: key, params: params});
+  }
+
 
 export const createModule = <T, A>(
   options: ReactorModuleOptions<T, A>
 ): ReactorModule<T, A> => {
   const state$ = new BehaviorSubject(options.initialState);
-  const actionFunctionCreator = createActionFunction(state$);
+  const action$ = new ReplaySubject();
+  const actionFunctionCreator = createActionFunction(state$, action$);
   const actions: ActionObject<A> = Object.fromEntries(
     Object.keys(options.actions).map((key) => [
       key,
-      actionFunctionCreator(options.actions[key as keyof A]),
+      actionFunctionCreator(key, options.actions[key as keyof A]),
     ])
   ) as any;
   return {
